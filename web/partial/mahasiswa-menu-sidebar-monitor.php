2560cec1ca2771dd483c87a2651cbf4a91bf0056
z3phyru5 <z3phyru5@users.noreply.github.com>
2022-07-12 18:44:48 +0700

    Change 1.1

--- web/partial/mahasiswa-menu-sidebar-monitor.php
+++ web/partial/mahasiswa-menu-sidebar-monitor.php
@@ -1,13 +1,14 @@
-
-<div></div>
-<div class="pos-fixed l-20 t-80 d-none d-lg-block z-index-10">
+<link href="../../assets/css/box.css" rel="stylesheet">
+<div class="pos-fixed t-60 d-none d-lg-block z-index-10">
+<div class="box1">
     <ul class="list-group">
-        <li class="list-group-item-its"><a href="../beranda" class="btn btn-icon btn-its-list rounded-its-10"><img src="../../assets/img/home.svg" class="wd-20"><span class="tx-poppins tx-medium mg-l-15 span-hide">Beranda</span></a></li>
-        <li class="list-group-item-its"><a href="../keuangan" class="btn btn-icon btn-its-list rounded-its-10"><img src="../../assets/img/icon-app/ITS-Tarif.svg" class="wd-20"><span class="tx-poppins tx-medium mg-l-15 span-hide">Keuangan</span></a></li>
-        <li class="list-group-item-its"><a href="../data-referensi" class="btn btn-icon btn-its-list rounded-its-10"><img src="../../assets/img/folder.svg" class="wd-20"><span class="tx-poppins tx-medium mg-l-15 span-hide">Data Referensi</span></a></li>
-        <li class="list-group-item-its"><a href="../laporan-perencanaan" class="btn btn-icon btn-its-list rounded-its-10"><img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20"><span class="tx-poppins tx-medium mg-l-15 span-hide">Laporan</span></a></li>
-        <li class="list-group-item-its"><a href="../pengaturan" class="btn btn-icon btn-its-list rounded-its-10"><img src="../../assets/img/setting.svg" class="wd-20"><span class="tx-poppins tx-medium mg-l-15 span-hide">Pengaturan</span></a></li>
+        <li class="list-group-item-its" style="left:20px"><a href="../beranda" class="btn btn-icon btn-its-list rounded-its-10"><img src="../../assets/img/home.svg" class="wd-20"><span class="tx-poppins tx-medium span-hide">Beranda</span></a></li>
+        <li class="list-group-item-its" style="left:20px"><a href="../keuangan" class="btn btn-icon btn-its-list rounded-its-10"><img src="../../assets/img/icon-app/ITS-Tarif.svg" class="wd-20"><span class="tx-poppins tx-medium span-hide">Keuangan</span></a></li>
+        <li class="list-group-item-its" style="left:20px"><a href="../data-referensi" class="btn btn-icon btn-its-list rounded-its-10"><img src="../../assets/img/folder.svg" class="wd-20"><span class="tx-poppins tx-medium span-hide">Data Referensi</span></a></li>
+        <li class="list-group-item-its" style="left:20px"><a href="../laporan-perencanaan" class="btn btn-icon btn-its-list rounded-its-10"><img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20"><span class="tx-poppins tx-medium span-hide">Laporan</span></a></li>
+        <li class="list-group-item-its" style="left:20px"><a href="../pengaturan" class="btn btn-icon btn-its-list rounded-its-10"><img src="../../assets/img/setting.svg" class="wd-20"><span class="tx-poppins tx-medium span-hide">Pengaturan</span></a></li>
         <!-- Apabila menu terlalu banyak, hapus beberapa menu dan gunakan ini. -->
         <!-- <li class="list-group-item-its"><a href="#menu-sidebar" class="btn btn-icon btn-its-list rounded-its-10 off-canvas-menu"><img src="../../assets/img/more.svg" class="wd-20"><span class="tx-poppins tx-medium mg-l-15 span-hide">Menu lainnya</span></a></li> -->
     </ul>
+</div>
 </div>
