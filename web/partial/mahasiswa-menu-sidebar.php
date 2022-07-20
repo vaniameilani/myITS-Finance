4413625058912ee0a4e54cd21e529fc99780e707
z3phyru5 <65481428+z3phyru5@users.noreply.github.com>
2022-07-20 12:10:59 +0700

    Change 2.0

--- web/partial/mahasiswa-menu-sidebar.php
+++ web/partial/mahasiswa-menu-sidebar.php
@@ -1,3 +1,4 @@
+<link href="../../assets/css/dropdown.css" rel="stylesheet">
 <div id="menu-sidebar" class="off-canvas off-canvas-overlay wd-300 wd-md-400 shadow">
     <div class="off-canvas-header bd-b-0">
     <a href="../beranda" class="aside-logo d-flex justify-content-center align-items-center">
@@ -9,11 +10,199 @@
     </div>
     <div class="off-canvas-body pd-t-5 pd-md-t-10">
         <ul class="sidebar-nav">
-            <li class="nav-item"><a href="../beranda" class="nav-link-its d-flex justify-content-start align-items-center"><img src="../../assets/img/home.svg" class="wd-20 mg-r-15"><span class="tx-poppins tx-medium text-truncate">Beranda</span></a></li>
-            <li class="nav-item"><a href="../keuangan" class="nav-link-its d-flex justify-content-start align-items-center"><img src="../../assets/img/icon-app/ITS-Tarif.svg" class="wd-20 mg-r-15"><span class="tx-poppins tx-medium text-truncate">Keuangan</span></a></li>
-            <li class="nav-item"><a href="../data-referensi"class="nav-link-its d-flex justify-content-start align-items-center"><img src="../../assets/img/folder.svg" class="wd-20 mg-r-15"><span class="tx-poppins tx-medium text-truncate">Data Referensi</span></a></li>
-            <li class="nav-item"><a href="../laporan-perencanaan" class="nav-link-its d-flex justify-content-start align-items-center"><img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20 mg-r-15"><span class="tx-poppins tx-medium text-truncate">Laporan</span></a></li>
-            <li class="nav-item"><a href="../pengaturan" class="nav-link-its d-flex justify-content-start align-items-center"><img src="../../assets/img/setting.svg" class="wd-20 mg-r-15"><span class="tx-poppins tx-medium text-truncate">Pengaturan</span></a></li>
+            <li class="nav-item mg-l-5"><a href="../beranda" class="nav-link-its d-flex justify-content-start align-items-center"><img src="../../assets/img/home.svg" class="wd-20 mg-r-15"><span class="tx-poppins tx-medium text-truncate">Beranda</span></a></li>
+            <li class="nav-item">
+                <button class="btn1"><a href="../keuangan" class="nav-link-its d-flex justify-content-start align-items-center">
+                    <img src="../../assets/img/icon-app/ITS-Tarif.svg" class="wd-20 mg-r-15">
+                    <span class="tx-poppins tx-medium text-truncate">Keuangan</span></a></button>
+                <button class="dropdown1-btn">
+                </button>
+                    <div class="dropdown1-container">
+                        <ul class="list-unstyled">
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/daftar-anggaran.svg" class="wd-20 mg-r-15">Daftar Mata Anggaran</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/icon-app/ITS-Tarif.svg" class="wd-20 mg-r-15">Daftar Mata Anggaran (Unit)</a></li>
+                            <li class="nav-item">
+                            <button class="btn1"><a href="#" class="nav-link-its d-flex justify-content-start align-items-center">          
+                                <img src="../../assets/img/icon-app/ITS-Tarif.svg" class="wd-20 mg-r-15">
+                                <span class="text-truncate">Anggaran</span></button></a>
+                                <button class="dropdown1-btn">
+                                </button>
+                                <div class="dropdown1-container">
+                                    <ul class="list-unstyled">
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/icon-app/ITS-Tarif.svg" class="wd-20 mg-r-15">Daftar Standar Satuan RBA</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/pagu-unit.svg" class="wd-20 mg-r-15">Pagu Unit</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/rba.svg" class="wd-20 mg-r-15">Daftar RBA</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/icon-app/ITS-Tarif.svg" class="wd-20 mg-r-15">Rencana Pendapatan</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/upah-minimum.svg" class="wd-20 mg-r-15">Pengajuan UMK</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/upah-minimum.svg" class="wd-20 mg-r-15">Pengembalian UMK</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/transfer-alokasi.svg" class="wd-20 mg-r-15">Transfer Alokasi</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/icon-app/ITS-Tarif.svg" class="wd-20 mg-r-15">Terima Alokasi</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/approve-rup.svg" class="wd-20 mg-r-15">Approve RUP</a></li>
+                        </ul>
+                    </div>
+                </li>      
+            </li>
+            <li class="nav-item">
+                <button class="btn1"><a href="../data-referensi"class="nav-link-its d-flex justify-content-start align-items-center">
+                    <img src="../../assets/img/folder.svg" class="wd-20 mg-r-15">
+                    <span class="tx-poppins tx-medium text-truncate">Data Referensi</span></a></button>
+                <button class="dropdown1-btn">
+                    </button>
+                    <div class="dropdown1-container">
+                        <ul class="list-unstyled">
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/folder.svg" class="wd-20 mg-r-15">Unit</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/folder.svg" class="wd-20 mg-r-15">Tahun Anggaran</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/upah-minimum.svg" class="wd-20 mg-r-15">Sumber Dana</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/satuan.svg" class="wd-20 mg-r-15">Standar Satuan</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/satuan.svg" class="wd-20 mg-r-15">Satuan</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/folder.svg" class="wd-20 mg-r-15">Program Kerja</a></li>
+                        </ul>
+                    </div>
+                </li>      
+            </li>
+            <li class="nav-item">
+                <button class="btn1"><a href="../laporan-perencanaan"class="nav-link-its d-flex justify-content-start align-items-center">
+                    <img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20 mg-r-15">
+                    <span class="tx-poppins tx-medium text-truncate">Laporan</span></a></button>
+                <button class="dropdown1-btn">
+                    </button>
+                    <div class="dropdown1-container">
+                        <ul class="list-unstyled">
+                            <li class="nav-item">
+                            <button class="btn1"><a href="#" class="nav-link-its d-flex justify-content-start align-items-center">          
+                                <img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20 mg-r-15">
+                                <span class="text-truncate">Tabel-Tabel</span></button></a>
+                                <button class="dropdown1-btn">
+                                </button>
+                                <div class="dropdown1-container">
+                                    <ul class="list-unstyled">
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20 mg-r-15">Tabel 1</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20 mg-r-15">Tabel 2</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20 mg-r-15">Tabel 3</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20 mg-r-15">Tabel 4</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                            <button class="btn1"><a href="#" class="nav-link-its d-flex justify-content-start align-items-center">          
+                                <img src="../../assets/img/finance/approve-rup.svg" class="wd-20 mg-r-15">
+                                <span class="text-truncate">Laporan RUP</span></button></a>
+                                <button class="dropdown1-btn">
+                                </button>
+                                <div class="dropdown1-container">
+                                    <ul class="list-unstyled">
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/approve-rup.svg" class="wd-20 mg-r-15">Laporan RUP 1</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/approve-rup.svg" class="wd-20 mg-r-15">Laporan RUP 2</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/approve-rup.svg" class="wd-20 mg-r-15">Laporan RUP 3</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/approve-rup.svg" class="wd-20 mg-r-15">Laporan RUP 4</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/approve-rup.svg" class="wd-20 mg-r-15">Laporan RUP 5</a></li>
+                                    </ul>    
+                                </div>                                
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20 mg-r-15">Efektivitas Anggaran</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20 mg-r-15">Diagram Efektifitas Anggaran</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20 mg-r-15">Rekapitulasi RKAT</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/rba.svg" class="wd-20 mg-r-15">RBA Detail</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its" >
+                                <img src="../../assets/img/icon-app/Manajemen-SK.svg" class="wd-20 mg-r-15">Pagu Tahunan</a></li>
+                            <li class="nav-item mg-l-5">
+                            <button class="btn1"><a href="#" class="nav-link-its d-flex justify-content-start align-items-center">          
+                                <img src="../../assets/img/finance/monitoring.svg" class="wd-20 mg-r-15">
+                                <span class="text-truncate">Monitoring</span></button></a>
+                                <button class="dropdown1-btn">
+                                </button>
+                                <div class="dropdown1-container">
+                                    <ul class="list-unstyled">
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/monitoring.svg" class="wd-20 mg-r-15">Dashboard Monitoring</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/monitoring.svg" class="wd-20 mg-r-15">Serapan Pagu Unit</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/monitoring.svg" class="wd-20 mg-r-15">Serapan Anggaran per MAK</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/monitoring.svg" class="wd-20 mg-r-15">Serapan Anggaran per Kegiatan</a></li>
+                                        <li class="nav-item"><a href="#" class="nav-link-its">
+                                            <img src="../../assets/img/finance/monitoring.svg" class="wd-20 mg-r-15">Serapan Anggaran per MA</a></li>
+                                    </ul>    
+                                </div>
+                               
+                    </div>
+                </li>      
+            </li>
+            <li class="nav-item">
+                <button class="btn1"><a href="../pengaturan"class="nav-link-its d-flex justify-content-start align-items-center">
+                    <img src="../../assets/img/setting.svg" class="wd-20 mg-r-15">
+                    <span class="tx-poppins tx-medium text-truncate">Pengaturan</span></a></button>
+                <button class="dropdown1-btn">
+                    </button>
+                    <div class="dropdown1-container">
+                        <ul class="list-unstyled">
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/set-kunci.svg" class="wd-20 mg-r-15">Pengaturan Kunci RBA</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/set-kunci.svg" class="wd-20 mg-r-15">Pengaturan Kunci RUP</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/finance/set-kunci.svg" class="wd-20 mg-r-15">Pengaturan Kunci SBI</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/setting.svg" class="wd-20 mg-r-15">Setting RBA</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/setting.svg" class="wd-20 mg-r-15">Pengumuman</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/setting.svg" class="wd-20 mg-r-15">Setting Unit Penunjang (AMU)</a></li>
+                            <li class="nav-item mg-l-5"><a href="#" class="nav-link-its">
+                                <img src="../../assets/img/setting.svg" class="wd-20 mg-r-15">Setting Unit Penunjang (LAB)</a></li>
+                        </ul>
+                    </div>
+                      
+            </li>           
         </ul>
     </div>
+<script>
+    var dropdown1 = document.getElementsByClassName("dropdown1-btn");
+    var i;
+
+    for (i = 0; i < dropdown1.length; i++) {
+        dropdown1[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdown1Content = this.nextElementSibling;
+    if (dropdown1Content.style.display === "block") {
+        dropdown1Content.style.display = "none";
+    } else {
+        dropdown1Content.style.display = "block";
+        }
+    });
+    }
+</script>
 </div><!-- off-canvas -->
